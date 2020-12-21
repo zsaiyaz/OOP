0ba115da46b5c0afa8d4716ef64ee495fbde5da0
ITDSIU19049 <69238956+zsaiyaz@users.noreply.github.com>
2020-12-21 12:17:12 +0700

    Add files via upload

---
+++ TETRIS_PROJECT-master/Tetris/src/Window.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+
+public class Window {
+
+    public static final int WIDTH = 450 , HEIGHT = 637 ;
+    private JFrame window;
+    private Board board;
+    private Title title;
+
+    public Window() {
+
+        window = new JFrame("TETRIS GAME");
+        window.setSize(WIDTH,HEIGHT);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+
+        board = new Board();
+        title = new Title(this);
+
+        window.addKeyListener(board);
+        window.addMouseMotionListener(title);
+        window.addMouseListener(title);
+
+        window.add(title);
+
+        window.setVisible(true);
+
+    }
+
+    public void startTetris()
+    {
+        window.remove(title);
+        window.addMouseMotionListener(board);
+        window.addMouseListener(board);
+        window.add(board);
+        board.startGame();
+        window.revalidate();
+    }
+
+    public static void main (String[] args) {
+        new Window();
+    }
+}
